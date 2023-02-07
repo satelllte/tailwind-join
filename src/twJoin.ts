f39a9ff15ace7e1856805823d770021f585af435
satelllte <lunaerxs@gmail.com>
2023-02-07 23:58:35 +0000

    && operator conditions

--- src/twJoin.ts
+++ src/twJoin.ts
@@ -1,9 +1,10 @@
-type InputToken = string | null | undefined
+type InputToken = string | false | null | undefined
 
 export const twJoin = (
   ...inputTokens: InputToken[]
 ): string => {
   return inputTokens
+    .filter(Boolean)
     .join(' ')
     .replaceAll(/  +/g, ' ')
     .trim()
