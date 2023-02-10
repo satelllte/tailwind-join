115b46d090d318a15b5477d59942831f4f73a2cc
satelllte <lunaerxs@gmail.com>
2023-02-10 20:45:00 +0000

    typing

--- src/twJoin.ts
+++ src/twJoin.ts
@@ -1,11 +1,11 @@
 type InputToken = string | false | null | undefined
 
-export const twJoin = (...args: InputToken[]) => {
+export const twJoin = (...args: InputToken[]): string => {
   let tmp: InputToken
   let str: string = ''
-  let x: string = ''
-  let i = 0
-  while (i < args.length) {
+  let x: string
+  let i: number = 0
+	while (i < args.length) {
 		if (tmp = args[i++]) {
 			if (x = tmp || '') {
 				str && (str += ' ')
